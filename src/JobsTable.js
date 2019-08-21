314024bf6c6e23c84700bc71572debe98b4f328c
Pirouz Mehmandoost <pirouz.sf@gmail.com>
2019-08-21 10:45:20 -0700

    Removed redundant methods in JobsTable.js in order to resort to usign sorting functionalities built in to react bootstrakc table 2.

--- src/JobsTable.js
+++ src/JobsTable.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './JobsTable.css';
 
-
-
 //TODO: Make sorting in ascending and descending order order and sorting by date work.
+//Maybe use React Bootstrap table 2 to do so.
 export default class JobsTable extends React.Component {
   constructor(props) {
     super(props);
@@ -13,69 +12,12 @@ export default class JobsTable extends React.Component {
         order: 'descendingOrder'
       }
     }
-
-    this.onSort = this.onSort.bind(this);
-    this.sort = this.sortColumn.bind(this);
-  }
-
-  onSort(event) {
-    // columns, 0 through 4
-    // alert(Object.values(event.target)[0].key);
-    const newIndex = Object.values(event.target)[0].key; //doesnt handly sorting by date properly.
-    const { index, order } = this.state.sort;
-    if (newIndex === index) {
-      switch (order) {
-        case 'descendingOrder':
-          this.setState({
-            sort: {
-              index,
-              order: 'ascendingOrder'
-            }
-          });
-          break;
-        case 'ascendingOrder':
-          this.setState({
-            sort: {
-              index: null,
-              order: 'descendingOrder'
-            }
-          });
-          break;
-        default:
-          this.setState({ sort:
-          {
-            index: newIndex,
-            order: 'descendingOrder'
-          }
-        });
-      }
-    }
-  }
-
-//Sorting doesn't work at the moment.
-  sortColumn(positions, columns) {
-    const { index, order } = this.state.sort;
-    if (index) {
-      const sortByProp = columns[index].prop;
-      const sortFunc = (f, g) => {
-        if (f[sortByProp] === g[sortByProp]) {
-          return 0;
-        }
-        return f[sortByProp].toUpperCase() > g[sortByProp].toUpperCase() ? 1 : -1;
-      }
-      let sortedPositions = [...positions].sort(sortFunc);
-      if (order === 'ascendingOrder') {
-        sortedPositions.reverse()
-      }
-      return sortedPositions;
-    } else {
-        return positions;
-    }
   }
 
   render() {
     const columns = this.props.columns;
-    const positions = this.sortColumn(this.props.positions, columns);
+    const positions = this.props.positions;
+
     return (
       <div className = "JobsTable">
         <table id = "table">
@@ -86,7 +28,6 @@ export default class JobsTable extends React.Component {
                 return (
                   <th
                     key = { index }
-                    onClick = { this.onSort } //3 ways to sort?  WIP
                   >
                   { column.name }
                   </th>
